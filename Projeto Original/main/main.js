d84ba5bb31d702dde30c89a63f3711368bcbcfd6
lttst4r <estelareginagomes014@gmail.com>
2026-02-21 11:01:34 -0300

    css

--- Projeto Original/main/main.js
+++ Projeto Original/main/main.js
@@ -128,6 +128,7 @@ function processarCliqueBotao(clickedButton, todasBotoes) {
     const OutSelected = document.querySelector('input[name="Out"]:checked')
     const OutEl = OutSelected ? OutSelected.value : ''
 
+    // mantém o mapeamento natural das cores
     if(InEl === 'blue'){
         In = 'azul'
         console.log('In Azul')
@@ -141,6 +142,7 @@ function processarCliqueBotao(clickedButton, todasBotoes) {
         console.log('Nulo')
     }
 
+    // mantém a correspondência de cor original para saídas
     if(OutEl === 'blue'){
         Out = 'azul'
         console.log('Out Azul')
